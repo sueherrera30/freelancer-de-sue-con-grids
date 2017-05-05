085e050bea87ecab62d5c34a3d54736a525ae8fa
sue herrera <sueherrera30@gmail.com>
2017-05-05 01:41:46 -0500

    maquetación completa con hovers y barra funcional

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-/*
- * Archivo principal de JS
- */
+
+// navegador
+var navegador =document.getElementById('barra');
+document.addEventListener("scroll",bajar);
+
+function bajar(){
+  var distancia = window.scrollY;
+  if(distancia>100) {
+  navegador.style.padding= 15 + "px";
+  }
+ else {
+  navegador.style.padding= 30 + "px";
+ }
+}
+
+// FORMULARIO
+var telefono = document.getElementById('telefono');
+telefono.addEventListener("click",aparecer);
+
+function aparecer(){
+  if(typeof telefono !== "number"){
+    alert("esta entrada no es válida");
+  }
+}
